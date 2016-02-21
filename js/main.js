b711a179df9fdd14fa95cb7e4bd1def8e9cfc7f9
chrissiexyz <[chrissie327@hotmail.com]>
2016-02-21 15:23:27 -0500

    Adding pages

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Color vector has been defined in bubbles.js file as below
+//var red = [0, 100, 63];
+//var orange = [40, 100, 60];
+//var green = [75, 100, 40];
+//var blue = [196, 77, 55];
+//var purple = [280, 50, 60];
+var message = "I Love You"
+var letterColors = [red, orange, green, blue, purple];
+drawName(message, letterColors);
+bubbleShape = 'circle';
+bounceBubbles();
